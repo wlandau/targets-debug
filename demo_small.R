11d7175cce1cddd98d0ee7100437b9893d8d54e6
wlandau-lilly <will.landau@lilly.com>
2025-04-17 13:00:02 -0400

    update demo_small.R

--- demo_small.R
+++ demo_small.R
@@ -12,7 +12,7 @@ tar_edit()
 tar_visnetwork()
 
 # Run the pipeline.
-tar_make_clustermq(workers = 1) # error
+tar_make() # error
 
 # Find the error.
 tar_visnetwork()
@@ -26,14 +26,11 @@ tar_meta(fields = error, complete_only = TRUE)
 tar_meta(fields = warnings, complete_only = TRUE)
 
 # random number generator seeds
-tar_meta(name = dataset1, field = seed)
+tar_meta(name = data, field = seed)
 tar_meta(name = model, field = seed)
 
 # Peel back layers to find more clues!
-# Can I reproduce the error without {clustermq}?
-tar_make() # same error
-
-# What about without {callr}?
+# Can I reproduce the error without {callr}?
 rstudioapi::restartSession() # remove detritus from the environment
 library(targets)
 tar_make(callr_function = NULL) # same error
@@ -41,8 +38,8 @@ tar_make(callr_function = NULL) # same error
 # What about just {callr} without {targets}?
 callr::r( # same error
   func = function() {
-    set.seed(-1012558151) # from tar_meta(name = dataset1, field = seed)
     library(targets)
+    set.seed(tar_meta(name = data, field = seed)$seed)
     suppressMessages(tar_load_globals())
     data <- simulate_data(units = 100)
     analyze_data(data)
@@ -50,39 +47,7 @@ callr::r( # same error
   show = TRUE
 )
 
-# What about {clustermq} without {targets}?
-# (if you used tar_make_clustermq())
-options(clustermq.scheduler = "multiprocess")
-clustermq::Q( # same error
-  fun = function(index) {
-    set.seed(-1012558151)
-    library(targets)
-    suppressMessages(tar_load_globals())
-    data <- simulate_data(units = 100)
-    analyze_data(data)
-  },
-  index = 1,
-  n_jobs = 1
-)
-
-# What about {future} without {targets}?
-# (if you used tar_make_future())
-future::plan(future::multisession)
-f <- future::future( # same error
-  expr = {
-    set.seed(-1012558151)
-    library(targets)
-    suppressMessages(tar_load_globals())
-    data <- simulate_data(units = 100)
-    analyze_data(data)
-  },
-  seed = TRUE
-)
-future::value(f)
-
-# What if we remove {targets}, {callr}, {clustermq}, and {future}
-# and just work with the custom R code?
-
+# What about in a simple interactive session?
 # Load functions, other globals, and packages.
 rm(list = ls())
 rstudioapi::restartSession()
@@ -92,18 +57,18 @@ suppressMessages(tar_load_globals())
 print(ls())
 
 # Reproduce the error using the function and the upstream data.
-tar_load(dataset1) # upstream data target
-analyze_data(dataset1) # same error
+tar_load(data) # upstream data target
+analyze_data(data) # same error
 
-# Careful: the error disappears for some random datasets.
-set.seed(0)
+# Start with the data this time, but use the same seed.
+set.seed(tar_meta(name = data, field = seed)$seed)
 random_data <- simulate_data(100)
 analyze_data(random_data)
 
 # But missing values in an object? Could it be a problem with dataset1?
-anyNA(dataset1$outcome)
+anyNA(data$outcome)
 
 # Try again.
-keep_units <- unique(dataset1$unit[!is.na(dataset1$outcome)])
-filtered_data <- filter(dataset1, unit %in% keep_units)
+keep_units <- unique(data$unit[!is.na(data$outcome)])
+filtered_data <- filter(data, unit %in% keep_units)
 analyze_data(filtered_data) # Solved!
